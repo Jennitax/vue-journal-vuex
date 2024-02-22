b8a0341bd7b49b498da08447894bee8421972278
Jennifer Romero <romerojennifer722@gmail.com>
2024-02-22 17:51:16 -0500

    Date entry

--- src/modules/daybook/components/EntryS.vue
+++ src/modules/daybook/components/EntryS.vue
@@ -1,23 +1,52 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({name: 'entry', params: {id:10 }})"
+    @click="$router.push({name: 'entry', params: {id: entry.id }})"
     >
         <!-- Tittle -->
         <div class="entry-title d-flex">
-            <span class="text-success fs-5 fw-bold">22</span>
-            <span class="mx-1 fs-5">Febrero</span>
-            <span class="mx-2 fw-light">2024, jueves</span>
+            <span class="text-success fs-5 fw-bold">{{day}}</span>
+            <span class="mx-1 fs-5">{{month}}</span>
+            <span class="mx-2 fw-light">{{yearDay}}</span>
         </div>
 
         <div class="entry-description">
-            Deserunt adipisicing eiusmod cillum deserunt dolore commodo nisi excepteur elit eu laboris. Ipsum sunt cupidatat eu aute pariatur irure in. Occaecat adipisicing do esse aute proident aliqua nisi nulla consectetur elit fugiat. Esse elit duis eiusmod irure. Nostrud occaecat elit ad quis laborum nisi mollit. Adipisicing consectetur aliquip sint ipsum Lorem eu reprehenderit eu velit in et. Magna elit excepteur est incididunt.
+            {{shortText}}
         </div>
     </div>
 </template>
 
 <script>
+
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+
 export default {
+    props: {
+        entry: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        shortText(){
+            return ( this.entry.text.length > 130)
+                ? this.entry.text.substring(0,130) + '...'
+                : this.entry.text
+        },
+        day(){
+            const date = new Date(this.entry.date)
+            return date.getDate()
+        },
+         month(){
+            const date = new Date(this.entry.date)
+            return months [date.getMonth()]
+        },
+        yearDay(){
+            const date = new Date(this.entry.date)
+            return `${date.getFullYear()}, ${days[date.getDay()]}`
+        }
+    }
 
 }
 </script>
