13036e28e2e6d53a91b1accd9328ac98e5a359b6
Jennifer Romero <romerojennifer722@gmail.com>
2024-02-26 20:07:39 -0500

    solved loadEntries data

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -8,6 +8,11 @@ export const loadEntries = async ({commit}) => {
     
     const {data} = await journalApi.get('/entries.json')
     
+if (!data){
+    commit ('setEntries', [])
+    return
+}
+
     const entries = []
     for (let id of Object.keys(data)){
         entries.push({
